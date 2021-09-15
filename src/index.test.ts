d7d93365a470d7409c38ddfedb124b7ecdb4d819
marcosvlehmkuhl <m.v.lehmkuhl@gmail.com>
2021-09-15 10:54:15 -0300

    WIP

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,11 @@
 import foo from "./";
+import { render } from "@testing-library/react";
 
 describe("GAME OF LIFE", () => {
-  test("test", () => {
-    expect(foo()).toEqual(10);
+  test("It should render a 8x4 grid", () => {
+    const { container, getByText } = render(<Grid />);
+    const expected = ".......\n.......\n.......\n.......";
+
+    expect(container.innerHTML).toEqual(expected);
   });
 });
